35f0dd0e963b0db9b14fc7664328f0744ce36b41
Purnachandra890 <131385647+Purnachandra890@users.noreply.github.com>
2025-08-24 13:08:02 +0530

    updated login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./Login";
 import Dashboard from "./Dashboard";
@@ -8,11 +9,29 @@ import ProblemList from "./ProblemList";
 import LeaderBoard from "./LeaderBoard";
 import ChatMessage from "./ChatMessage";
 import DashboardRoom from "./DashboardRoom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) return null; // or loading spinner
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        {/* <Route path="/" element={<LoginPage />} /> */}
+        <Route
+          path="/"
+          element={user ? <Navigate to="/dashboard" replace /> : <LoginPage />}
+        />
         <Route
           path="/dashboard"
           element={
